a7521ff4c3d92ad3f1a460018833385a365832e8
Bravo-Tango 72-74 <stanislaw.pendowski@infotech.edu.pl>
2025-05-13 21:04:04 +0200

    naprawione klika błędów

--- Lethal wiki/src/App.tsx
+++ Lethal wiki/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
